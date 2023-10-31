e19da8c7c9f7195305c8c9131bbe3fa2fe81203b
TheKid77 <andymcc77@blueyonder.co.uk>
2023-10-31 17:38:56 +0000

    Added dynamic variables

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const web_address="http://www.google.co.uk"
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edits <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <a href={web_address}>This is the google website</a>
+      </div>
     </div>
   );
 }
